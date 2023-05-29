40c7d9b47bf4d1d62327669505a4cfd660b78a3e
Chikimiko <cococandy333@gmail.com>
2023-05-28 19:02:38 -0700

    Finished

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,19 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('is #c4fdff a valid Hex color', () => {
+  expect(functions.isHexColor('#c4fdff')).toBe(true);
+});
+
+test('is #g9hdg7 a valid Hex color', () => {
+    expect(functions.isHexColor('#g9hdg7')).toBe(false);
+  });
+
+test('is Apple a strong password', () => {
+    expect(functions.isStrongPassword('Apple')).toBe(true);
+  });
+
+test('is isthisavalidpassword a strong password', () => {
+    expect(functions.isStrongPassword('isthisavalidpassword')).toBe(false);
+  });
